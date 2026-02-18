ad222c19da9bf4077f5741b9274d8c63be340d39
karenatalie <natalieekaren@gmail.com>
2026-02-18 11:59:27 +0700

    create a new pages

---
+++ src/pages/profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="page">
+      <h1>👤 Profile Page</h1>
+      <p>Ini adalah halaman profil user.</p>
+    </div>
+  );
+};
+
+export default Profile; 
