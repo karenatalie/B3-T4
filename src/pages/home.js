0cf8fb20d573c846e5e034fc7dd51d11688a6ac7
akhyafajar <akhyafajar871@gmail.com>
2026-02-19 11:42:51 +0700

    card sections

--- src/pages/home.js
+++ src/pages/home.js
@@ -41,25 +41,71 @@ const Home = () => {
 
         <div className="card-grid">
           <div className="card">
-            <img src="https://source.unsplash.com/400x300/?bali,beach" alt="Bali" />
+            <img src="/foto/indonesia.jpeg" alt="Bali" />
             <h3>Bali</h3>
             <p>Mulai dari Rp 850.000</p>
           </div>
 
           <div className="card">
-            <img src="https://source.unsplash.com/400x300/?maldives" alt="Maldives" />
-            <h3>Maldives</h3>
+            <img src="/foto/laos.jpeg" alt="Patuxai" />
+            <h3>Patuxai</h3>
             <p>Mulai dari Rp 5.200.000</p>
           </div>
 
           <div className="card">
-            <img src="https://source.unsplash.com/400x300/?phuket" alt="Phuket" />
-            <h3>Phuket</h3>
+            <img src="/foto/cambodia.jpeg" alt="Ankor Wat" />
+            <h3>Ankor Wat</h3>
             <p>Mulai dari Rp 3.400.000</p>
           </div>
 
           <div className="card">
-            <img src="https://source.unsplash.com/400x300/?hawaii" alt="Hawaii" />
+            <img src="/foto/myanmar.jpeg" alt="Schwedakong" />
+            <h3>Schwedakong</h3>
+            <p>Mulai dari Rp 7.100.000</p>
+          </div>
+
+          <div className="card">
+            <img src="/foto/brunei.jpeg" alt="Sultan Mosque" />
+            <h3>Sultan Mosque</h3>
+            <p>Mulai dari Rp 7.100.000</p>
+          </div>
+
+          <div className="card">
+            <img src="/foto/thailand.jpeg" alt="The Grand Palace" />
+            <h3>The Grand Palace</h3>
+            <p>Mulai dari Rp 7.100.000</p>
+          </div>
+          <div className="card">
+            <img src="/foto/malaysia.jpeg" alt="Hawaii" />
+            <h3>Kuala Lumpur</h3>
+            <p>Mulai dari Rp 7.100.000</p>
+          </div>
+
+          <div className="card">
+            <img src="/foto/pilipin.jpeg" alt="Hawaii" />
+            <h3>dimana ya?</h3>
+            <p>Mulai dari Rp 7.100.000</p>
+          </div>
+
+          <div className="card">
+            <img src="/foto/singa.jpeg" alt="Hawaii" />
+            <h3>Singapore</h3>
+            <p>Mulai dari Rp 7.100.000</p>
+          </div>
+          <div className="card">
+            <img src="/foto/myanmar.jpeg" alt="Hawaii" />
+            <h3>Hawaii</h3>
+            <p>Mulai dari Rp 7.100.000</p>
+          </div>
+
+          <div className="card">
+            <img src="/foto/myanmar.jpeg" alt="Hawaii" />
+            <h3>Hawaii</h3>
+            <p>Mulai dari Rp 7.100.000</p>
+          </div>
+
+          <div className="card">
+            <img src="/foto/myanmar.jpeg" alt="Hawaii" />
             <h3>Hawaii</h3>
             <p>Mulai dari Rp 7.100.000</p>
           </div>
