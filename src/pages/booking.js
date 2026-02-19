927cdd3053f2c8bc82d2c5eedb9ad5309831f763
Izzaalvaro <72863585+Izzaalvaro@users.noreply.github.com>
2026-02-19 16:11:13 +0700

    tata cara Booking

--- src/pages/booking.js
+++ src/pages/booking.js
@@ -13,7 +13,19 @@ const Booking = () => {
 
   return (
     <div className="page">
-      <h1>📝 Booking</h1>
+      <h1>📅 Booking</h1>
+      <h2>How to Book Flights with B3-TA</h2>
+      <h1>1.<u>Search Your Flight</u></h1>
+      <h2>Start your search by filling in the flight details.</h2>
+      <h1>2.<u>Choose and Book Flight</u></h1>
+      <h2>Airline name, flight schedule, and ticket price will be shown on the search results page. Choose and book the best flight for you.</h2>
+      <h1>3.<u>Fill In Contact Information and Passenger Details</u></h1>
+      <h2>After choosing your flight, fill in the contact information and passenger details.</h2>
+      <h2>You can add travel insurance to your booking as well.</h2>
+      <h1>4.<u>Complete Your Payment</u></h1>
+      <h2>From the many payment methods available, choose one that best suits you.</h2>
+      <h1>5.<u>Receive Your E-ticket</u></h1>
+      <h2>Within 60 minutes after your payment is confirmed, we will send your e-ticket to your email.</h2>
       <div className="input-box">
         <input value={newTitle} onChange={(e) => setNewTitle(e.target.value)} />
         <button onClick={() => { dispatch(addManual(newTitle)); setNewTitle(''); }}>Tambah</button>
